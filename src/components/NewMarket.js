e5eaa42fa4d941c69c8ccc76eb8aee78740c8835
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2020-04-27 09:04:56 +1000

    feat(NewMarket): add tags

--- src/components/NewMarket.js
+++ src/components/NewMarket.js
@@ -7,6 +7,9 @@ import { UserContext } from "../App";
 
 class NewMarket extends React.Component {
   state = {
+    tags: ["Arts", "Technology", "Crafts", "Entertainment", "Webdev"],
+    selectedTags: [],
+    options: [],
     addMarketDialog: false,
     name: "",
   };
@@ -14,12 +17,16 @@ class NewMarket extends React.Component {
   handleAddMarket = async (user) => {
     try {
       this.setState({ addMarketDialog: false });
-      const input = { name: this.state.name, owner: user.username };
+      const input = {
+        name: this.state.name,
+        tags: this.state.selectedTags,
+        owner: user.username,
+      };
       const result = await API.graphql(
         graphqlOperation(createMarket, { input })
       );
       console.info(`Created market id: ${result.data.createMarket.id}`);
-      this.setState({ name: "" });
+      this.setState({ name: "", selectedTags: [] });
     } catch (err) {
       console.error("Couldn't add market:", err);
       Notification.error({
@@ -27,6 +34,13 @@ class NewMarket extends React.Component {
         message: `${err.message || "Error adding market"}`,
       });
     }
+  };
+
+  handleFilterTags = (query) => {
+    const options = this.state.tags
+      .map((tag) => ({ value: tag, label: tag }))
+      .filter((tag) => tag.label.toLowerCase().includes(query.toLowerCase()));
+    this.setState({ options });
   };
 
   render() {
@@ -62,6 +76,26 @@ class NewMarket extends React.Component {
                       onChange={(name) => this.setState({ name })}
                       value={this.state.name}
                     />
+                  </Form.Item>
+                  <Form.Item label="Add Tags">
+                    <Select
+                      multiple={true}
+                      filterable={true}
+                      placeholder="Market Tags"
+                      onChange={(selectedTags) =>
+                        this.setState({ selectedTags })
+                      }
+                      remoteMethod={this.handleFilterTags}
+                      remote={true}
+                    >
+                      {this.state.options.map((option) => (
+                        <Select.Option
+                          key={option.value}
+                          label={option.label}
+                          value={option.value}
+                        />
+                      ))}
+                    </Select>
                   </Form.Item>
                 </Form>
               </Dialog.Body>
